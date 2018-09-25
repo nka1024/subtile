a37a23025f1eb65b1ff21816bd58425baa8f333f
nka1024 <nka1024@gmail.com>
2018-09-25 12:24:51 +0700

    delete deploy scripts

--- deploy-all.sh
+++
@@ -1,6 +0,0 @@
-echo 'Backing up node_modules..'
-mv ./node_modules ../node_modules_subtile_backup
-scp -i "vps1.pem" -r ./ ubuntu@ec2-18-216-219-185.us-east-2.compute.amazonaws.com:pong/
-echo 'Restore node_modules...'
-mv ../node_modules_subtile_backup ./node_modules
-echo 'Done.'
--- deploy-server.sh
+++
@@ -1,1 +0,0 @@
-scp -i "vps1.pem" -r ./server.js ubuntu@ec2-18-216-219-185.us-east-2.compute.amazonaws.com:pong/
